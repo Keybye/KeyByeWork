54c77a4458ca2ecbfe697b913a1b2f10d696b55c
Keybye <2295657647@qq.com>
2023-09-18 19:56:31 +0800

    Initial commit

--- js/main.js
+++ js/main.js
@@ -1,13 +1,1 @@
-const main = document.querySelector('.main')
-Array(3)
-    .fill(null)
-    .forEach(()=>{
-        main.insertAdjacentHTML("beforeend", '<div>a</div>')
-    })
-function change(){
-    var div=document.getElementById("top");
-    var w=parseInt(div.style.width);
-    var h=parseInt(div.style.height);
-    div.style.width="30%";
-    div.style.height="50%";
-}
+<link/>
