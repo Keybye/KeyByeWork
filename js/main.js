a20d6947bfd41a6cfd7c440730cf14f247cc243c
Keybye <2295657647@qq.com>
2023-09-18 20:43:56 +0800

    Revert "Initial commit"

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
-<link/>
+const main = document.querySelector('.main')
+Array(3)
+    .fill(null)
+    .forEach(()=>{
+        main.insertAdjacentHTML("beforeend", '<div>a</div>')
+    })
+function change(){
+    var div=document.getElementById("top");
+    var w=parseInt(div.style.width);
+    var h=parseInt(div.style.height);
+    div.style.width="30%";
+    div.style.height="50%";
+}
